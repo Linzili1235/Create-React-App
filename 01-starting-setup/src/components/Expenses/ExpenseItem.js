177a8629e00db6bddcab72ef4dc3c9c8dc4cb6e2
Linzili1235 <jq138@georgetown.edu>
2022-10-13 11:20:26 -0400

    previous files

---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+//just a function
+//returns html code
+import React,{useState} from "react";
+
+
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props){
+    // const [title, setTitle] = useState(props.title);//react hook
+
+    // // on clickHandler when you click, the function will run
+    // const clickHandler = ()=> {
+    //     setTitle('Updated!');
+    //     console.log(title);
+    // };
+    return (
+        <Card className={'expense-item'}>
+            <ExpenseDate date={props.date}/>
+            <div  className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
+
+}
+
+export default ExpenseItem;
